53f7fcaddf09f62bb02979dcb252f7cf00f36668
JuliaSABEL <sabelnikova.j.partner@gmail.com>
2024-04-17 20:29:33 +0300

    Mechanics of moving the enemy mushroom

--- Pik the Frog/Assets/Scripts/EnemyMovement.cs
+++ Pik the Frog/Assets/Scripts/EnemyMovement.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private Collider2D enemyCollider;
-    public float movementSpeed;
-    private bool newDirection = true;
+    public float speed = 3f;
+    public LayerMask labyrinthLayer;
+
+    private Rigidbody2D rb;
+    private bool moveRight = true;
 
     void Start()
     {
-        enemyCollider = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
+
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
-        if (enemyCollider.IsTouchingLayers(LayerMask.GetMask("Labyrinth")) && newDirection)
+        if (moveRight)
         {
-            Debug.Log("Touch");
-            newDirection = false;
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (labyrinthLayer == (labyrinthLayer | (1 << collision.gameObject.layer)))
+        {
+            moveRight = !moveRight;
         }
     }
 }
